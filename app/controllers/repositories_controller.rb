945d5010f24085d2a928a6a0cb70e3536d53a825
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-21 16:16:26 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -10,6 +10,10 @@ class RepositoriesController < ApplicationController
       req.params['client_secret'] = '6620a1071ba5ae6a4c9fbf908b472e9a119b9580'
       req.params['q'] = params[:query]
     end
+    json = JSON.parse(@resp.body)
+    @html_url = json['items'][0]['html_url']
+    @description = json['items'][0]['description']
+    @name = json['items'][0]['name']
     render 'search'
   end
 end
