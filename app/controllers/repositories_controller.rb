152dd5874302cf5c005c2967a31e4a962e0419a1
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-21 15:12:09 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,6 +6,6 @@ class RepositoriesController < ApplicationController
 
   def github_search
     byebug
-    @resp = Faraday.get 'https://api.github.com/search/repositories'
+    @resp = Faraday.get 'https://api.github.com/search/repositories',{:g => params[:query]}
   end
 end
