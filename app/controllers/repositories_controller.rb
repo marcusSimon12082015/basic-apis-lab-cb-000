29ef1501bb248af464f13f9859c9704b77308a43
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-21 16:03:03 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,6 +4,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    byebug
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = '57c52ba3af1436288e9d'
       req.params['client_secret'] = '6620a1071ba5ae6a4c9fbf908b472e9a119b9580'
