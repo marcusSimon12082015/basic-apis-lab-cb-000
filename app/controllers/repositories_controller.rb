80fa7eb797ae3b9fe3c5273f8c506f378565fb9d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-05-21 15:53:10 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,7 @@ class RepositoriesController < ApplicationController
       req.params['client_id'] = '57c52ba3af1436288e9d'
       req.params['client_secret'] = '6620a1071ba5ae6a4c9fbf908b472e9a119b9580'
       req.params['q'] = params[:query]
-    end 
+    end
     byebug
   end
 
